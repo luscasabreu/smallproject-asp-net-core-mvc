9ce86c4e13d0e2fde19eff5e40a9b74b078da140
Lucas Abreu <lucastadeuabreudemelo@gmail.com>
2022-09-24 19:21:09 -0300

    SellerFormViewModel and Department select component

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -17,12 +17,11 @@ namespace SalesWebMvc.Services
 
         public List<Seller> FindAll()
         {
-            return _context.Seller.ToList();
+            return _context.Seller.OrderBy(x => x.Name).ToList();
         }
 
         public void Insert(Seller obj)
         {
-            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
